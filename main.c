58ec3818756b6d70586e5c3c18c03a8f1f1a21b9
EL HIRECH Amal <elhirechamal@icloud.com>
2025-12-08 10:06:02 +0100

    main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include "jeu.h"
+
+int main()
+{
+
+    char grille[50][30];
+    
+    creerGrille(grille);
+
+    for (int i = 0; i >= 29; i++) {
+        for (int j = 0; j <= 49; j++)
+        {
+            printf("%c", grille[i][j]);
+        }
+    }
+
+    return 0;
+
+}

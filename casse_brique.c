2d36b56f8d6fc4e6f40b537ccf31e4116302e76a
EL HIRECH Amal <elhirechamal@icloud.com>
2025-12-08 11:08:10 +0100

    jacques

--- casse_brique.c
+++ casse_brique.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include "jeu.h"
 
-void creerGrille (char grille[50][30])
+void creerGrille (char grille[15][20])
 {
-    for(int i=0; i<= 49; i++)
+    for(int i=0; i<= 14; i++)
     {
-        for (int j = 0; j<=29; j++)
+        for (int j = 0; j<=19; j++)
         {
             if(i == 0)
             {
                 grille[0][j] = '#';    
             }
-            else 
+            else
             {
                 grille[i][j] = ' ';
             }
@@ -19,4 +19,31 @@ void creerGrille (char grille[50][30])
         }
     }
 
+    grille[14][5] = '=';
+    grille[14][6] = '=';
+    grille[14][7] = '=';
+    grille[14][8] = '=';
+
+
+
 }
+
+/*void plateforme(char grille[50][30])
+{
+    char plateforme[4][1] = {"=","=","=","="};
+    char choix;
+    int idx;
+    scanf("%d", &choix)
+    if (choix == 'd')
+    {
+        for(int j = 0; j<=49; j++)
+        {
+            idx = idx + 1
+        }
+        else 
+        {
+            idx = idx - 1
+        }
+    }
+    
+}*/
--- jeu.h
+++ jeu.h
@@ -2,6 +2,6 @@
 #define JEU
 #include <stdio.h>
 
-void creerGrille (char grille[50][30]);
-
+void creerGrille (char grille[15][20]);
+/*void plateforme(char grille[50][30]);*/
 #endif 

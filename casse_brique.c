89e1d47f22e465c307571d6e2487f5d8a7ec1543
EL HIRECH Amal <elhirechamal@icloud.com>
2025-12-08 10:05:28 +0100

    casse_brique

--- casse_brique.c
+++ casse_brique.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 #include "jeu.h"
 
+void creerGrille (char grille[50][30])
+{
+    for(int i=0; i<= 29; i++)
+    {
+        for (int j = 0; j<=49; j++)
+        {
+            grille[i][j] = '#';
+        }
+    }
+
+}

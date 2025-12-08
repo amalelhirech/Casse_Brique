49cdeacb175bdbffb77ec0d985376173c3342975
EL HIRECH Amal <elhirechamal@icloud.com>
2025-12-08 14:00:21 +0100

    casse_brique

--- casse_brique.c
+++ casse_brique.c
@@ -29,7 +29,7 @@ void creerGrille (char grille[15][20])
 void plateforme(char grille[15][20])
 {
     char choix;
-    scanf(" %c", &choix);  
+    scanf(" %c", &choix);
 
     int ligne = 14;
     int debut = -1;
@@ -48,17 +48,17 @@ void plateforme(char grille[15][20])
     grille[ligne][debut + 2] = ' ';
     grille[ligne][debut + 3] = ' ';
 
-    if (choix == 'd')
-        debut++;
-    else if (choix == 'g')
+    if (choix == 'g' && debut > 0)
         debut--;
+
+    if (choix == 'd' && debut + 3 < 19)
+        debut++;
 
     grille[ligne][debut] = '=';
     grille[ligne][debut + 1] = '=';
     grille[ligne][debut + 2] = '=';
     grille[ligne][debut + 3] = '=';
 }
-
 
 void deplacerBalle(char grille[15][20],int balleColonne, int balleLigne,int directionX, int directionY,int resultat[4])
 {
@@ -85,4 +85,12 @@ void deplacerBalle(char grille[15][20],int balleColonne, int balleLigne,int dire
     resultat[1] = y;
     resultat[2] = directionX;
     resultat[3] = directionY;
+}
+
+int perdu(int balleLigne)
+{
+    if (balleLigne > 14)
+        return 1;  
+
+    return 0;       
 }
